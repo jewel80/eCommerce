ca2f2a500b8ef8f63961148d063971a56c9fef1f
Md. Jewel Rana <m.jewelrana80@gmail.com>
2021-12-04 01:24:12 +0600

    database connect and project architecture

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const app = express();
 
+
+
+
+
+app.use(express.json());
+
+
+//Import all routes
+const products = require('./routes/peoduct');
+
+
+
+app.use('/api/v1', products)
+
+
+
+
+
+
+
 module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 const app = require('./app')
-
+const connectDatabase = require('./config/database')
 const dotenv = require('dotenv');
 
 
-//setteing up config file
+
+
+//setteing up config file 
 dotenv.config({ path: 'backend/config/config.env' })
 
 app.listen(process.env.port, () => {
-    console.log(`Backend Server started on port: ${process.env.port}`);
+    console.log(`Backend Server started on port: ${process.env.port} in ${process.env.NODE_ENV}`);
 })
+
+
+//connecting to dabase 
+connectDatabase();
+
+
+
+app.get('/a', function(req, res, next) {
+    console.log('req.body'); // not a string, but your parsed JSON data
+    console.log('req.body.a'); // etc.
+    // ...
+});
