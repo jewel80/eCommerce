f62d6d7823a7dd57694f98c80c18194a91c43a3b
Md. Jewel Rana <m.jewelrana80@gmail.com>
2021-12-04 21:31:57 +0600

    Adding Products Resource

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,81 @@
-exports.getProducts = (req, res, next) => {
+const Product = require('../models/product');
+
+
+//create new product => /api/v1/product/new
+exports.newProduct = async(req, res, next) => {
+    // console.log(JSON.parse(JSON.stringify(req.body)));
+    const product = await Product.create(req.body)
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+
+//Get All Product => /api/v1/admin/products
+exports.getProducts = async(req, res, next) => {
+    const products = await Product.find();
     res.status(200).json({
         success: true,
-        message: "This route will show all products in DB"
+        count: products.length,
+        products
+    })
+}
+
+
+//Get Single Product => /api/v1/product/:id
+exports.getSingleProduct = async(req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'product not found'
+        })
+    }
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+
+//Update Product => /api/v1/admin/product/:id
+exports.updateProduct = async(req, res, next) => {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'product not found'
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+
+//Delete Product => /api/v1/admin/product/:id
+exports.deleteProduct = async(req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'product not found'
+        })
+    }
+
+    await product.remove()
+
+    res.status(200).json({
+        success: true,
+        message: 'Product is deleted.'
     })
 }
---
+++ backend/models/product.js
@@ -0,0 +1,93 @@
+const Mongoose = require('mongoose');
+// var app = require('mongoose');
+
+
+const productSchema = new Mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'please enter product name'],
+        trim: true,
+        maxLength: [100, 'prodcut name cannnot exceed 100 charcters']
+    },
+    price: {
+        type: Number,
+        required: [true, 'please enter product price'],
+        trim: true,
+        maxLength: [5, 'prodcut price cannnot exceed 5 charcters'],
+        default: 0.0
+    },
+    description: {
+        type: String,
+        required: [true, 'please enter product description']
+    },
+    ratings: {
+        type: Number,
+        default: 0
+    },
+    images: [{
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    }],
+    category: {
+        type: String,
+        required: [true, 'please enter product category'],
+        enum: {
+            values: [
+                'Electronic',
+                'Cameras',
+                'Laptop',
+                'Accessoris',
+                'Headphone',
+                'Food',
+                'Clothe/Shoes',
+                'Beauty/Health',
+                'Sports',
+                'outdoor',
+                'Home'
+            ],
+            message: 'please select category for product'
+        }
+    },
+    seller: {
+        type: String,
+        required: [true, 'please enter product seller']
+    },
+    stock: {
+        type: Number,
+        required: [true, 'please enter product stock'],
+        maxLength: [5, 'prodcut name cannnot exceed 5 charcters'],
+        default: 0
+    },
+    numberOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [{
+        name: {
+            type: String,
+            required: true
+        },
+        rating: {
+            type: String,
+            required: true
+        },
+        comment: {
+            type: String,
+            required: true
+        },
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+
+module.exports = Mongoose.model('product', productSchema);
