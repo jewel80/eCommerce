93e2b59444fe77544e807d94212b0e2ca7a7969f
jewel rana <m.jewelrana80@gmail.com>
2021-12-29 12:21:00 +0600

    Users & Authentication backend and Frontend

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,17 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser')
-// const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload')
 const cloudinary = require('cloudinary')
 
 
 const errorMiddleware = require('./middlewares/errors')
 
 
-
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(fileUpload());
 
 
 //Setting up clodeinary 
