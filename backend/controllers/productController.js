2f1366df8fd98760fe24311c3c7bc6ea3aca4af9
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-01-29 23:06:38 +0600

    update function of productController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,53 +5,50 @@ const ErrorHandler = require('../utils/errorHandler')
 const APIFeatures = require('../utils/apiFeatures')
 const cloudinary = require('cloudinary')
 
-
-// Create new product   =>   /api/v1/admin/product/new
-// exports.newProduct = catchAsyncErrors(async (req, res, next) => {
-
-//     let images = []
-//     if (typeof req.body.images === 'string') {
-//         images.push(req.body.images)
-//     } else {
-//         images = req.body.images
-//     }
-
-//     let imagesLinks = [];
-
-//     for (let i = 0; i < images.length; i++) {
-//         const result = await cloudinary.v2.uploader.upload(images[i], {
-//             folder: 'products'
-//         });
-
-//         imagesLinks.push({
-//             public_id: result.public_id,
-//             url: result.secure_url
-//         })
-//     }
-
-//     req.body.images = imagesLinks
-//     req.body.user = req.user.id;
-
-//     const product = await Product.create(req.body);
-
-//     res.status(201).json({
-//         success: true,
-//         product
-//     })
-// })
-
 // create new product => /api/v1/product/new
-exports.newProduct = catchAsyncErrors(async(req, res, next) => {
+exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
+    let images = []
+    if (typeof req.body.images === 'string') {
+        images.push(req.body.images)
+    } else {
+        images = req.body.images
+    }
+
+    let imagesLinks = [];
+
+    for (let i = 0; i < images.length; i++) {
+        const result = await cloudinary.v2.uploader.upload(images[i], {
+            folder: 'products'
+        });
+
+        imagesLinks.push({
+            public_id: result.public_id,
+            url: result.secure_url
+        })
+    }
+
+    req.body.images = imagesLinks
     req.body.user = req.user.id;
 
-    const apiFeatures = new APIFeatures(Product.find(), )
-    const product = await Product.create(req.body)
+    const product = await Product.create(req.body);
+
     res.status(201).json({
-        // success: true,
+        success: true,
         product
     })
 })
+// exports.newProduct = catchAsyncErrors(async(req, res, next) => {
+
+//     req.body.user = req.user.id;
+
+//     const apiFeatures = new APIFeatures(Product.find(), )
+//     const product = await Product.create(req.body)
+//     res.status(201).json({
+//         // success: true,
+//         product
+//     })
+// })
 
 //Get all Admin Product => /api/v1/admin/products..
 exports.getAdminProducts = catchAsyncErrors(async(req, res, next) => {
@@ -101,31 +98,8 @@ exports.getSingleProduct = catchAsyncErrors(async(req, res, next) => {
 })
 
 
-//Update Product => /api/v1/admin/product/:id for test
-exports.updateProduct = catchAsyncErrors(async(req, res, next) => {
-    console.log(req.body);
-    let product = await Product.findById(req.params.id);
-    if (!product) {
-        return next(new ErrorHandler('Product not found', 404))
-    }
-
-    
-
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-        useFindAndModify: false
-    })
-
-    res.status(200).json({
-        success: true,
-        product
-    })
-})
-
-
 // Update Product   =>   /api/v1/admin/product/:id
-exports.updateProductgggg = catchAsyncErrors(async (req, res, next) => {
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
 
     let product = await Product.findById(req.params.id);
 
@@ -179,25 +153,27 @@ exports.updateProductgggg = catchAsyncErrors(async (req, res, next) => {
 
 })
 
-
 //Delete Product => /api/v1/admin/product/:id
-exports.deleteProduct = catchAsyncErrors(async(req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+
     const product = await Product.findById(req.params.id);
+
     if (!product) {
-        return next(new ErrorHandler('Product not found', 404))
+        return next(new ErrorHandler('Product not found', 404));
     }
 
-    // // Deleting images associated with the product
-    // for (let i = 0; i < product.images.length; i++) {
-    //     const result = await cloudinary.v2.uploader.destroy(product.images[i].public_id)
-    // }
+    // Deleting images associated with the product
+    for (let i = 0; i < product.images.length; i++) {
+        const result = await cloudinary.v2.uploader.destroy(product.images[i].public_id)
+    }
 
-    await product.remove()
+    await product.remove();
 
     res.status(200).json({
         success: true,
         message: 'Product is deleted.'
     })
+
 })
 
 
