5b112fc3973da60fea26d187530128080c41ec6b
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-01-30 22:04:56 +0600

    DB file update

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,7 +7,6 @@ const connectDatabase = () => {
     // mongoose.connect(process.env.DB_LOCAL_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
-            // useCreateIndex: true
     }).then(con => {
         console.log(`MongoDB Database connected with HOST: ${con.connection.host}`);
     })
